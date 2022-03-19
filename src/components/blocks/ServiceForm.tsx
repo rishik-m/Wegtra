5cb01ffd2a4013e6c048b715532e40e4b1fd327e
Rhythm Shandlya <armaanbgp@gmail.com>
2022-03-19 23:45:11 +0530

    form

--- src/components/blocks/ServiceForm.tsx
+++ src/components/blocks/ServiceForm.tsx
@@ -7,64 +7,57 @@ const ServiceForm = () => (
                 <div className="form-content">
                     <div className="form-items">
                         <h3>Find Battery Of Your Performance</h3>
-                        <br />
+                        <div className="col-md-12 mt-3">
+                            <label className="mb-3 mr-1" htmlFor="gender">Vehicle Type: </label>
+
+                            <input type="radio" className="btn-check" name="gender" id="male" autoComplete="off" required />
+                            <label className="btn" htmlFor="male">Car</label>
+
+                            <input type="radio" className="btn-check" name="gender" id="female" autoComplete="off" required />
+                            <label className="btn" htmlFor="female">Bike</label>
+
+                            <input type="radio" className="btn-check" name="gender" id="secret" autoComplete="off" required />
+                            <label className="btn" htmlFor="secret">Truck</label>
+                            <div className="valid-feedback mv-up">You selected vehicle!</div>
+                            <div className="invalid-feedback mv-up">Please select a vehicle!</div>
+                        </div>
                         <p>Search By Modal: </p>
                         <form className="requires-validation">
                             <div className="col-md-12">
-                                <input className="form-control" type="text" name="name" placeholder="Full Name" required />
-                                <div className="valid-feedback">Username field is valid!</div>
-                                <div className="invalid-feedback">Username field cannot be blank!</div>
+                                <input className="form-control" type="text" name="name" placeholder="Keywords" />
                             </div>
-
-                            <div className="col-md-12">
-                                <input className="form-control" type="email" name="email" placeholder="E-mail Address" required />
-                                <div className="valid-feedback">Email field is valid!</div>
-                                <div className="invalid-feedback">Email field cannot be blank!</div>
-                            </div>
-
                             <div className="col-md-12">
                                 <select className="form-select mt-3" required>
-                                    <option selected disabled value="">Position</option>
-                                    <option value="jweb">Junior Web Developer</option>
-                                    <option value="sweb">Senior Web Developer</option>
-                                    <option value="pmanager">Project Manager</option>
+                                    <option selected disabled value="">Brand</option>
+                                    <option value="jweb">Maruti Suzuki</option>
+                                    <option value="sweb">MRF</option>
+                                    <option value="pmanager">Tarzen</option>
                                 </select>
-                                <div className="valid-feedback">You selected a position!</div>
-                                <div className="invalid-feedback">Please select a position!</div>
+                                <div className="valid-feedback">You selected a brand!</div>
+                                <div className="invalid-feedback">Please select a brand!</div>
                             </div>
-
-
                             <div className="col-md-12">
-                                <input className="form-control" type="password" name="password" placeholder="Password" required />
-                                <div className="valid-feedback">Password field is valid!</div>
-                                <div className="invalid-feedback">Password field cannot be blank!</div>
+                                <select className="form-select mt-3" required>
+                                    <option selected disabled value="">Modal</option>
+                                    <option value="jweb">MX2103</option>
+                                    <option value="sweb">UI20</option>
+                                    <option value="pmanager">MRF506</option>
+                                </select>
+                                <div className="valid-feedback">You selected a modal!</div>
+                                <div className="invalid-feedback">Please select a modal</div>
                             </div>
-
-
-                            <div className="col-md-12 mt-3">
-                                <label className="mb-3 mr-1" htmlFor="gender">Gender: </label>
-
-                                <input type="radio" className="btn-check" name="gender" id="male" autoComplete="off" required />
-                                <label className="btn btn-sm btn-outline-secondary" htmlFor="male">Male</label>
-
-                                <input type="radio" className="btn-check" name="gender" id="female" autoComplete="off" required />
-                                <label className="btn btn-sm btn-outline-secondary" htmlFor="female">Female</label>
-
-                                <input type="radio" className="btn-check" name="gender" id="secret" autoComplete="off" required />
-                                <label className="btn btn-sm btn-outline-secondary" htmlFor="secret">Secret</label>
-                                <div className="valid-feedback mv-up">You selected a gender!</div>
-                                <div className="invalid-feedback mv-up">Please select a gender!</div>
+                            <div className="col-md-12">
+                                <select className="form-select mt-3" required>
+                                    <option selected disabled value="">Varient</option>
+                                    <option value="jweb">VAR-1</option>
+                                    <option value="sweb">VAR-2</option>
+                                    <option value="pmanager">VAR-3</option>
+                                </select>
+                                <div className="valid-feedback">You selected a varient!</div>
+                                <div className="invalid-feedback">Please select a varient!</div>
                             </div>
-
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required />
-                                <label className="form-check-label">I confirm that all data are correct</label>
-                                <div className="invalid-feedback">Please confirm that the entered data are all correct!</div>
-                            </div>
-
-
                             <div className="form-button mt-3">
-                                <button id="submit" type="submit" className="btn btn-primary">Register</button>
+                                <button id="submit" type="submit" className="btn btn-primary">Search Now</button>
                             </div>
                         </form>
                     </div>
